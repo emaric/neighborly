5922ab10fdb0016000f09f5a015e20a456bf85d8
Maric E. <emaric.dev@gmail.com>
2025-08-14 10:36:21 +0800

    :rocket: :construction: use express and cord deployment to fix cors

---
+++ client/auth-app/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors({ origin: '*' })); // or specify shell app origin
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Remote running on port ${process.env.PORT || 3001}`);
+});
