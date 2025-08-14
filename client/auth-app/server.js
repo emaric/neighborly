c77107a5e8b3b705614c8e9134e9099e47395a94
Maric E. <emaric.dev@gmail.com>
2025-08-14 10:51:30 +0800

    :rocket: :construction: test set url as cors origin

--- client/auth-app/server.js
+++ client/auth-app/server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({ origin: '*' })); // or specify shell app origin
+app.use(cors({ origin: 'https://neighborly-app.onrender.com' })); // or specify shell app origin
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(process.env.PORT || 3001, () => {
